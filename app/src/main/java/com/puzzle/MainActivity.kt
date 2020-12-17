ee69bd7114186500d926c8407e3d736d1f9add18
李荣康 <lrk@meitu.com>
2020-12-17 17:31:04 +0800

    【优化】删除不必要的apply的使用

--- app/src/main/java/com/puzzle/MainActivity.kt
+++ app/src/main/java/com/puzzle/MainActivity.kt
@@ -23,6 +23,7 @@ import com.puzzle.adappter.TemplateAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.layout_template.view.*
 import kotlinx.android.synthetic.main.layout_title.*
+import kotlinx.android.synthetic.main.layout_title.view.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -31,28 +32,32 @@ import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
+    private val frameIconHeight = 40
     private var showTemplate = true
     private var shouldUpdateTabLayout = false
-    private var currentFrameMode = R.drawable.meitu_puzzle__frame_none to "无边框"
-    private var images = emptyList<String>()
+    private val templateCategoryNum = 6
     private var selectNum = 1
     private var imageHeight = 0
     private var imageWidth = 0
+    private var currentFrameMode = R.drawable.meitu_puzzle__frame_none to "无边框"
+    private var images = emptyList<String>()
     private val template2CategoryMap = TemplateData.templateInCategory(selectNum)
     private val fistTemplateInCategoryMap = TemplateData.templateCategoryFirst(selectNum)
     private val templateRecyclerViewLayoutManager = LinearLayoutManager(this).apply {
         orientation = LinearLayoutManager.HORIZONTAL
     }
-    private val templateAdapter = TemplateAdapter(
-        TemplateData.allTemplateWithPictureNum(selectNum)
-    ) { adapter, holder ->
-        templateGroup.templateTabLayout.setScrollPosition(
-            template2CategoryMap[holder.adapterPosition] ?: 0,
-            0f,
-            false
-        )
-        adapter.selectPosition = holder.adapterPosition
-        adapter.notifyDataSetChanged()
+    private val templateAdapter by lazy {
+        TemplateAdapter(
+            TemplateData.allTemplateWithPictureNum(selectNum)
+        ) { adapter, holder ->
+            templateGroup.templateTabLayout.setScrollPosition(
+                template2CategoryMap[holder.adapterPosition] ?: 0,
+                0f,
+                false
+            )
+            adapter.selectPosition = holder.adapterPosition
+            adapter.notifyDataSetChanged()
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -74,108 +79,112 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initTemplateViewGroup() {
-        templateGroup.apply {
-            templateRecyclerView.apply {
-                adapter = templateAdapter
-                layoutManager = templateRecyclerViewLayoutManager
-                setOnScrollChangeListener { _, _, _, _, _ ->
-                    if (shouldUpdateTabLayout) {
-                        val lastPos =
-                            templateRecyclerViewLayoutManager.findLastVisibleItemPosition()
-                        val pos = if (lastPos == templateAdapter.list.size - 1) {
-                            5
-                        } else {
-                            val firstPos =
-                                templateRecyclerViewLayoutManager.findFirstVisibleItemPosition()
-                            template2CategoryMap[firstPos] ?: 0
-                        }
-                        templateGroup.templateTabLayout.setScrollPosition(
-                            pos, 0F, false
-                        )
-                    }
-                    shouldUpdateTabLayout = true
-                }
-            }
-            templateTabLayout.apply {
-                addTab(newTab().setIcon(R.drawable.meitu_puzzle_temp_34))
-                addTab(newTab().setIcon(R.drawable.meitu_puzzle_temp_11))
-                addTab(newTab().setIcon(R.drawable.meitu_puzzle_temp_43))
-                addTab(newTab().setIcon(R.drawable.meitu_puzzle_temp_169))
-                addTab(newTab().setIcon(R.drawable.meitu_puzzle_temp_full))
-                addTab(newTab().setIcon(R.drawable.meitu_puzzle_temp_others))
-                addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-                    override fun onTabSelected(tab: TabLayout.Tab?) {
-                        shouldUpdateTabLayout = false
-                        val categoryPos = (tab?.position ?: 0)
-                        templateRecyclerViewLayoutManager.scrollToPositionWithOffset(
-                            fistTemplateInCategoryMap[categoryPos] ?: 0,
-                            0
-                        )
-                    }
+        initTemplateRecyclerView()
+        initTemplateTabLayout()
+        initCloseImageView()
+        initFrameModeView()
+    }
 
-                    override fun onTabUnselected(tab: TabLayout.Tab?) {
-
-                    }
-
-                    override fun onTabReselected(tab: TabLayout.Tab?) {
-                    }
-                })
-            }
-            frameTextView.apply {
-                val drawable = ContextCompat.getDrawable(
-                    this@MainActivity,
-                    R.drawable.meitu_puzzle__frame_none
-                )?.apply {
-                    setBounds(0, 0, dp2px(40), dp2px(40))
-                }
-                setCompoundDrawables(null, drawable, null, null)
-                setOnClickListener {
-                    updateFrameMode()
-                }
-            }
-            closeImageView.apply {
-                setOnTouchListener { _, event ->
-                    if (event.action == MotionEvent.ACTION_UP) {
-                        showTemplate = !showTemplate
-                        val res = if (showTemplate) {
-                            R.drawable.ic_down
-                        } else {
-                            R.drawable.ic_up
-                        }
-                        performClick()
-                        setImageResource(res)
-                    }
-                    true
-                }
-            }
+    private fun initFrameModeView() {
+        val drawable = ContextCompat.getDrawable(this, R.drawable.meitu_puzzle__frame_none)?.apply {
+            setBounds(0, 0, frameIconHeight.dp2px(), frameIconHeight.dp2px())
         }
+        templateGroup.frameTextView.setCompoundDrawables(null, drawable, null, null)
+        templateGroup.frameTextView.setOnClickListener {
+            updateFrameMode()
+        }
+    }
 
+    private fun initCloseImageView() {
+        closeImageView.setOnTouchListener { view, event ->
+            if (event.action == MotionEvent.ACTION_UP) {
+                showTemplate = !showTemplate
+                val res = if (showTemplate) {
+                    R.drawable.ic_down
+                } else {
+                    R.drawable.ic_up
+                }
+                view.performClick()
+                closeImageView.setImageResource(res)
+            }
+            true
+        }
+    }
+
+    private fun initTemplateTabLayout() {
+        templateGroup.templateTabLayout.apply {
+
+            addTab(newTab().setIcon(R.drawable.meitu_puzzle_temp_34))
+            addTab(newTab().setIcon(R.drawable.meitu_puzzle_temp_11))
+            addTab(newTab().setIcon(R.drawable.meitu_puzzle_temp_43))
+            addTab(newTab().setIcon(R.drawable.meitu_puzzle_temp_169))
+            addTab(newTab().setIcon(R.drawable.meitu_puzzle_temp_full))
+            addTab(newTab().setIcon(R.drawable.meitu_puzzle_temp_others))
+
+            addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+
+                override fun onTabSelected(tab: TabLayout.Tab) {
+                    shouldUpdateTabLayout = false
+                    val categoryPos = (tab.position)
+                    templateRecyclerViewLayoutManager.scrollToPositionWithOffset(
+                        fistTemplateInCategoryMap[categoryPos] ?: 0,
+                        0
+                    )
+                }
+
+                override fun onTabUnselected(tab: TabLayout.Tab) {}
+                override fun onTabReselected(tab: TabLayout.Tab) {}
+            })
+        }
+    }
+
+    private fun initTemplateRecyclerView() {
+
+        templateGroup.templateRecyclerView.adapter = templateAdapter
+        templateGroup.templateRecyclerView.layoutManager = templateRecyclerViewLayoutManager
+
+        templateGroup.templateRecyclerView.setOnScrollChangeListener { _, _, _, _, _ ->
+            if (shouldUpdateTabLayout) {
+                val lastPos =
+                    templateRecyclerViewLayoutManager.findLastVisibleItemPosition()
+                val pos = if (lastPos == templateAdapter.list.size - 1) {
+                    templateCategoryNum - 1
+                } else {
+                    val firstPos =
+                        templateRecyclerViewLayoutManager.findFirstVisibleItemPosition()
+                    template2CategoryMap[firstPos] ?: 0
+                }
+                templateGroup.templateTabLayout.setScrollPosition(
+                    pos, 0F, false
+                )
+            }
+            shouldUpdateTabLayout = true
+        }
     }
 
     private fun initTitleBar() {
-        titleBar.apply {
-            backImageView.setOnClickListener {
-                finish()
-            }
-            finishImageView.setOnClickListener {
-                saveBitmap(puzzleImageView, System.currentTimeMillis().toString())
-            }
+        titleBar.backImageView.setOnClickListener {
+            finish()
+        }
+        titleBar.finishImageView.setOnClickListener {
+            saveBitmap(puzzleImageView, System.currentTimeMillis().toString())
         }
     }
 
+
     private fun initBottomTabLayout() {
         bottomTabLayout.apply {
+
             addTab(newTab().setText("模板"))
             addTab(newTab().setText("海报"))
             addTab(newTab().setText("自由"))
             addTab(newTab().setText("拼接"))
+
             addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-                override fun onTabSelected(tab: TabLayout.Tab?) {
-                }
+                override fun onTabSelected(tab: TabLayout.Tab) {}
+                override fun onTabUnselected(tab: TabLayout.Tab) {}
 
-                override fun onTabUnselected(tab: TabLayout.Tab?) {}
-
-                override fun onTabReselected(tab: TabLayout.Tab?) {
+                override fun onTabReselected(tab: TabLayout.Tab) {
                     closeImageView.performClick()
                 }
             })
@@ -201,13 +210,11 @@ class MainActivity : AppCompatActivity() {
     private fun view2bitmap(view: View): Bitmap {
         val height = view.height
         val width = view.width
-        Log.e("kkl", "width $width height $height")
         val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
         val canvas = Canvas(bitmap)
 //        view.layout(0, dp2px(60), width, height)
 //        canvas.drawColor(Color.WHITE)
         view.draw(canvas)
-        Log.e("kkl", "width ${bitmap.width} height ${bitmap.height}")
         return bitmap
     }
 
@@ -251,17 +258,16 @@ class MainActivity : AppCompatActivity() {
             R.drawable.meitu_puzzle__frame_medium -> R.drawable.meitu_puzzle__frame_large to "大边框"
             else -> R.drawable.meitu_puzzle__frame_none to "无边框"
         }
-        templateGroup.frameTextView.apply {
-            text = currentFrameMode.second
-            setCompoundDrawables(
-                null,
-                ContextCompat.getDrawable(this@MainActivity, currentFrameMode.first)?.apply {
-                    setBounds(0, 0, dp2px(40), dp2px(40))
-                }, null, null
-            )
+
+        templateGroup.frameTextView.text = currentFrameMode.second
+        val drawable = ContextCompat.getDrawable(
+            this@MainActivity,
+            currentFrameMode.first
+        )?.apply {
+            setBounds(0, 0, frameIconHeight.dp2px(), frameIconHeight.dp2px())
         }
+        templateGroup.frameTextView.setCompoundDrawables(null, drawable, null, null)
     }
 
-    private fun dp2px(dp: Int) = (dp * resources.displayMetrics.density + 0.5f).toInt()
-
+    private fun Int.dp2px() = (this * resources.displayMetrics.density + 0.5f).toInt()
 }
